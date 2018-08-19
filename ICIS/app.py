0d076fa8d29a57e1c3f5c0e7de4262e490d94c01
KeitHLeE94 <wogus4187@naver.com>
2018-08-19 21:48:35 +0900

    특정 속성 지정하여 검색하는것은 가능. 속성 제한 없이 검색하려면 어떻게???

--- ICIS/app.py
+++ ICIS/app.py
@@ -1,11 +1,24 @@
+import elasticsearch
+import json
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+es_client = elasticsearch.Elasticsearch('localhost:9200')
+
+
+doc = es_client.search(index='_all', body={
+  "query": {
+    "match_phrase": {
+      "_id": "icitRmUBAESBoZrfMYmb"
+    }
+  }
+})
 
 
 @app.route('/')
 def index():
+    print(json.dumps(doc, ensure_ascii=False, indent=2))
     return render_template('index.html')
 
 
