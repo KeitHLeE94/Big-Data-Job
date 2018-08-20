5004b0fc7b4023fde0f29a5fbb94a9089198288f
KeitHLeE94 <wogus4187@naver.com>
2018-08-20 23:36:55 +0900

    shop-grid 페이지 추가

--- ICIS/app.py
+++ ICIS/app.py
@@ -10,7 +10,7 @@ es_client = elasticsearch.Elasticsearch('localhost:9200')
 doc = es_client.search(index='_all', body={
   "query": {
     "match_phrase": {
-      "_id": "icitRmUBAESBoZrfMYmb"
+      "title": "1단계"
     }
   }
 })
@@ -20,6 +20,11 @@ doc = es_client.search(index='_all', body={
 def index():
     print(json.dumps(doc, ensure_ascii=False, indent=2))
     return render_template('index.html')
+
+
+@app.route('/shop-grid')
+def shop_grid():
+    return render_template('shop-grid.html')
 
 
 if __name__ == '__main__':
