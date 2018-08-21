9c9c3e05b0d4a71e81c66a6ecbcfcc7625e7ee0e
KeitHLeE94 <wogus4187@naver.com>
2018-08-21 18:25:23 +0900

    mongodb, elastic search 연동 초안

--- ICIS/app.py
+++ ICIS/app.py
@@ -1,30 +1,48 @@
 import elasticsearch
 import json
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
 es_client = elasticsearch.Elasticsearch('localhost:9200')
 
 
-doc = es_client.search(index='_all', body={
-  "query": {
-    "match_phrase": {
-      "title": "1단계"
-    }
-  }
-})
-
-
 @app.route('/')
 def index():
-    print(json.dumps(doc, ensure_ascii=False, indent=2))
     return render_template('index.html')
 
 
 @app.route('/shop-grid')
 def shop_grid():
     return render_template('shop-grid.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    titles = []
+    imgs = []
+
+    search_term = request.form['search']
+    doc = es_client.search(index='_all', body={
+        "query": {
+            "match_phrase": {
+                "title": search_term
+            }
+        }
+    }, size=999)
+
+    resultCount = len(doc['hits']['hits'])
+
+    # for i in range(resultCount):
+    #     print(json.dumps(doc['hits']['hits'][i]['_source'], ensure_ascii=False, indent=2))
+
+    for i in range(resultCount):
+        titles.append(doc['hits']['hits'][i]['_source']['title'])
+
+    for i in range(resultCount):
+        imgs.append(doc['hits']['hits'][i]['_source']['img'])
+
+    return render_template('shop-grid.html', titles=titles, imgs=imgs)
 
 
 if __name__ == '__main__':
